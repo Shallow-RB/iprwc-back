df942c871a7fbd4d4592a0cf3f61799cfb36d83f
Shallow-RB <ryan.bhola2601@gmail.com>
2022-04-01 23:09:03 +0200

    pom

--- src/main/java/nl/ryanb/iprwcback/security/SecurityConfig.java
+++ src/main/java/nl/ryanb/iprwcback/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+//        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
